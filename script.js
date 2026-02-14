cdb2340f29ff16b02a454e6db421e65a9189dd9f
dev-groznov <devgroznov@gmail.com>
2026-02-14 17:04:02 +0300

    Code for the entire Etch-a-Sketch game
    Major changes:
    1. When the cursor enters a block, it turns blue.
    2. Two buttons have been added:
    a) The "Clear" button changes all block colors to white.
    b) The "Change the number of cells" button creates a request to change the number of blocks in a single line.

--- script.js
+++ script.js
@@ -1,14 +1,39 @@
-const conteiner = document.querySelector('.conteiner')
-
-for (let i = 0; i < 16; i++) {
-    const stringBlock = document.createElement('div')
-    stringBlock.classList.add('stringBlock')
-    conteiner.appendChild(stringBlock)
-    for (let j = 0; j < 16; j++) {
-        const block = document.createElement('div')
-        block.classList.add('block')
-        stringBlock.appendChild(block)
-        block.addEventListener('mouseenter', () =>
-            block.style.backgroundColor = 'blue')
+function getNewGrid(numberOfCells) {
+    for (let i = 0; i < numberOfCells; i++) {
+        const stringBlocks = document.createElement('div')
+        stringBlocks.classList.add('stringBlock')
+        conteiner.appendChild(stringBlocks)
+        for (let j = 0; j < numberOfCells; j++) {
+            const block = document.createElement('div')
+            block.classList.add('block')
+            stringBlocks.appendChild(block)
+            block.style.height = `${800 / numberOfCells}px`
+            block.addEventListener('mouseenter', () =>
+                block.style.backgroundColor = 'blue')
+        }
     }
 }
+
+const conteiner = document.querySelector('.conteiner')
+const clear = document.querySelector('.clear')
+const changeNumber = document.querySelector('.changeNumber')
+let newNumberOfCells = 16
+    
+clear.addEventListener('click', () => {
+    conteiner.innerHTML = ''
+    getNewGrid(newNumberOfCells)
+})
+
+changeNumber.addEventListener('click', () => {
+    while (true) {
+        newNumberOfCells = +prompt('Enter the number of cells (from 1 to 100)')
+        if (newNumberOfCells > 0 && newNumberOfCells <= 100) {
+            break
+        }
+    }
+    conteiner.innerHTML = ''
+    getNewGrid(newNumberOfCells)
+    
+})
+
+getNewGrid(newNumberOfCells)
